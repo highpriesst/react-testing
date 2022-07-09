0e522f91fe0bc1faf99e97d59afba82480598809
Ertan Mutlu <ertansofia@gmail.com>
2022-07-09 22:10:53 +0300

    css'in

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-red-500">React Testing</h1>
+      <div className="flex justify-center items-center gap-4 border border-l-cyan-800 h-screen">
+        <div className="border border-black h-96 p-5 bg-gray-200">
+          <h2>Testing 'Components'</h2>
+        </div>
+        <div className="border border-black h-96 p-5 bg-gray-200">
+          <h2>Results of the tests</h2>
+        </div>
+      </div>
     </div>
   );
 }
