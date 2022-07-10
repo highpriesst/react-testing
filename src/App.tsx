7b4a16724394fc57ed3e1807171efb0657e3fbb2
Ertan Mutlu <ertansofia@gmail.com>
2022-07-10 19:26:09 +0300

    tsx app changed

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="flex justify-center items-center gap-4 border border-l-cyan-800 h-screen">
-        <div className="border border-black h-96 p-5 bg-gray-200">
-          <h2>Testing 'Components'</h2>
-        </div>
-        <div className="border border-black h-96 p-5 bg-gray-200">
-          <h2>Results of the tests</h2>
-        </div>
-      </div>
+    <div className="App flex justify-center items-center h-screen">
+      <h1>react testings</h1>
     </div>
   );
 }
